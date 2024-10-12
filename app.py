ae374f4a683f763b41fc037d1bbb652fc43cb5c6
SM-AI -Enthusiast <85394665+SHASHWAT-MISHRA-997@users.noreply.github.com>
2024-10-12 21:12:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ emotion_pipeline = pipeline("text-classification", model="j-hartmann/emotion-eng
 
 # Initialize speech recognition and text-to-speech
 recognizer = sr.Recognizer()
-engine = pyttsx3.init()
+engine = pyttsx3.init(driverName='sapi5') 
 
 # Optionally set a voice
 voices = engine.getProperty('voices')
